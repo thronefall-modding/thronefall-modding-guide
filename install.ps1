0738881b1d8072632216132a4208dbb8601ce3e1
Rikhardur Bjarni Einarsson <badulf96@gmail.com>
2023-08-07 07:11:51 +0100

    Add a try throw to install so the build fails if the install script fails

--- install.ps1
+++ install.ps1
@@ -1,21 +1,25 @@
-$sln_path = $Args[0]
-$input_path = $Args[1]
+try {
+    $sln_path = $Args[0]
+    $input_path = $Args[1]
 
-$config = Get-Content (Join-Path $sln_path install.cfg) | ConvertFrom-StringData
+    $config = Get-Content (Join-Path $sln_path install.cfg) | ConvertFrom-StringData
 
-function CopyDll {
-    param (
-        $dll
+    function CopyDll {
+        param (
+            $dll
+        )
+
+        Write-Output "Installing $(Join-Path $config.InstallPath $dll)"
+        $null = New-Item -ItemType File -Path (Join-Path $config.InstallPath $dll) -Force
+        $null = Copy-Item (Join-Path $input_path $dll) (Join-Path $config.InstallPath $dll) -Force
+    }
+
+    $dlls = (
     )
 
-    Write-Output "Installing $(Join-Path $config.InstallPath $dll)"
-    $null = New-Item -ItemType File -Path (Join-Path $config.InstallPath $dll) -Force
-    $null = Copy-Item (Join-Path $input_path $dll) (Join-Path $config.InstallPath $dll) -Force
-}
-
-$dlls = (
-)
-
-$dlls | ForEach-Object {
-    CopyDll -dll $_
+    $dlls | ForEach-Object {
+        CopyDll -dll $_
+    }
+} catch {
+    throw 1
 }
